f512c52d4b8175fc25e9df99a9e78479a72eddb3
bb <bb@163.com>
2022-08-27 16:54:03 +0800

    '项目模块'

---
+++ gmall-wms/src/main/java/com/atguigu/gmall/wms/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.atguigu.gmall.wms.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        //设置请求的页面大于最大页后操作，true调回首页false继续请求  默认false
+        //paginationInterceptor.setOverflow(false)
+        //设置最大单页限制数量，默认500条， -1 不受限制
+        //paginationInterceptor.setLimit(500)
+        //开启count的join优化，只针对left join
+        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
+        return paginationInterceptor;
+    }
+
+
+}
